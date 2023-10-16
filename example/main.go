4fac82c0ad68b8ffa6c3d00ed44f6746f0cdde40
Rahul Baruri <rahulbaruri1@gmail.com>
2023-10-16 02:19:55 +0200

    Update documentation

--- example/main.go
+++ example/main.go
@@ -2,74 +2,74 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/rbrahul/retry"
 )
 
 func main() {
-	//i := 0
+	i := 0
 	var err error
-
-	// example 1: retry.Retry(retriableFn, maxRetry, delay)
-	/*fmt.Println("Example 1: retry.Retry(retriableFn, maxRetry, delay)")
-	retry.Retry(func() bool {
+	// Example: It will Retry infinitely with 1 second default delay until the operation succeeds
+	err = retry.Retry(func() bool {
 		defer func() {
 			i++
 		}()
-		fmt.Println("Hello")
-		return true
-	}, 10, 1*time.Second)*/
-
-	// example 2: retry.Retry(retriableFn, maxRetry, delayFunc)
-	/*err := retry.Retry(func() bool {
-		defer func() {
-			i++
-		}()
-		fmt.Println("Hello")
-		return true
-	}, 10, func() uint64 {
-		return 2
+		fmt.Println("I will be called in every second until I succeed")
+		return i < 10 // if i>=20 retry succeeds and will not retry further
 	})
+
+	if err == nil {
+		fmt.Println("Operation suceeded")
+	}
+
+	// Example: Retry maximum 5 times with 1 second interval
+	err = retry.Retry(func() bool {
+		fmt.Println("I will be called in every second but maximum 5 times")
+		return true
+	}, 5, 1*time.Second)
+
+	if err == retry.ErrMaximumRetryExceeded {
+		fmt.Println("RETRY ERROR:", retry.ErrMaximumRetryExceeded.Error())
+	}
+
+	// Example: Will be called until 10 seconds from now in every 2 seconds
+	err = retry.Retry(func() bool {
+		fmt.Println("I will be called until 10 seconds from now in every 2 seconds")
+		return true
+	}, 10*time.Second, 2*time.Second)
+
 	if err != nil {
 		fmt.Println("RETRY ERROR:", err)
-	}*/
-
-	// example 3: retry.Retry(retriableFn, maxRetry{int or time.Duration }, delay{time.Duration})
-
-	/*err = retry.Retry(func() bool {
-		defer func() {
-			i++
-		}()
-		fmt.Println("Hello")
-		return true
-	}, 10, 2*time.Second)
-
-	if err != nil {
-		fmt.Println("RETRY ERROR Example 3:", err)
-	}*/
-
-	/*t1 := time.Now()
-	err = retry.Retry(func() bool {
-		defer func() {
-			i++
-		}()
-		fmt.Println("Hello")
-		return i < 10
-	}, 25, 5*time.Second)
-	if err != nil {
-		fmt.Println("RETRY ERROR Example 3:", err)
 	}
-	diff := time.Now().Unix() - t1.Unix()
-	fmt.Println("DIFF:", diff)
-	*/
 
+	// Example: It will be called maxium 5 times with a Random interval between 2 to 5 seconds using the retry.RandomBackoff backoff function.
 	err = retry.Retry(func() bool {
-		fmt.Println("Hello")
+		fmt.Println("I will be called maxium 5 times with a random interval from 2 to 5 seconds")
 		return true
-	}, func(_ uint64) uint64 {
+	}, 5, retry.RandomBackoff(2, 5))
+	if err != nil {
+		fmt.Println("RETRY ERROR:", err)
+	}
+
+	// Example: It will be called maxium 5 times with an Exponential interval but maxium 10 seconds using the retry.ExponentialBackoff backoff function.
+	err = retry.Retry(func() bool {
+		fmt.Println("I will be called maxium 5 times with an Exponential interval but maximum delay will be 10 seconds")
+		return true
+	}, 5, retry.ExponentialBackoff(10))
+	if err != nil {
+		fmt.Println("RETRY ERROR:", err)
+	}
+
+	// Example: It will retry maxium 10 times with a Custom backoff function that returns 2 seconds delay
+	err = retry.Retry(func() bool {
+		fmt.Println("I will be called maxium 10 times in every 2 seconds")
+		return true
+	}, 10, func(_ uint64) uint64 {
 		return uint64(2)
 	})
+
 	if err != nil {
-		fmt.Println("RETRY ERROR Example 3:", err)
+		fmt.Println("RETRY ERROR:", err)
 	}
 }
